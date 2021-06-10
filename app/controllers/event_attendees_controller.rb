b73a8edcc15552a57ceab56b04150d8921ff8cd6
Arinpe <dehtolah@gmail.com>
2021-06-10 13:08:06 +0100

    Add event attendee association and list of attendees

---
+++ app/controllers/event_attendees_controller.rb
@@ -0,0 +1,13 @@
+class EventAttendeesController < ApplicationController
+    def create
+        event = Event.find(params[:event_id])
+        if EventAttendee.where(attendee_id: current_user.id, attended_event_id: params[:event_id]).exists?
+            redirect_to event_path(event) 
+            return
+        end
+        @event_attendee = event.event_attendees.build(attendee_id: current_user.id)
+         if @event_attendee.save
+              redirect_to event_path(event) 
+         end
+    end
+end
---
+++ db/migrate/20210610114849_create_event_attendee.rb
@@ -0,0 +1,8 @@
+class CreateEventAttendee < ActiveRecord::Migration[6.1]
+  def change
+    create_table :event_attendees do |t|
+
+      t.timestamps
+    end
+  end
+end
